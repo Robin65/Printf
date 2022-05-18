938993e1480067338985bc1a110cf865814f3772
chris7695 <cschwennig@gmx.de>
2022-05-18 19:57:34 +0200

    Erstellung der format.c Datei

---
+++ format.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int d=7695;
+float f=7695.99;
+char c='H';
+char s[]="Huhu";
+
+
+int main ()
+    {
+	    printf ("Ganzzahl: %d \n", d);
+	    printf ("Gleitkommazahl: %F \n", f);
+	    printf ("Character: %c \n", c);
+	    printf ("String: %s \n", s);
+	    return 0;
+
+}
