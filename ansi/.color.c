650b3facb2f23b0ee71a35c0b6d154cc09d9e573
Robin65 <robinchristian.heyer@stud.thga.de>
2022-05-19 08:29:53 -0500

    ADD_A4

---
+++ ansi/.color.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main();
+
+printf ("\x1b[32m   _____  _____   __  \n ");
+printf ("\x1b[33m  |  __ \|  __ \ / /  \n ");
+printf ("\x1b[34m  | |__) | |__) / /_  \n ");
+printf ("\x1b[35m  |  ___/|  ___/ '_ \ \n ");
+printf ("\x1b[36m  | |    | |   | (_) |\n "); 
+printf ("\x1b[37m  |_|    |_|    \___/ \n ");
+
+	return 0;	
